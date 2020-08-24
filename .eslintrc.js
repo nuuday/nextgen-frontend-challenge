4bd1556765d00dee644da66f9560ca26b26e5e82
Andreas Vercell <anv@tdc.dk>
2020-08-24 10:43:05 +0200

    Update all modules + Linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
+    "prettier/react",
   ],
+  plugins: ["react", "react-hooks", "prettier"],
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: "module",
+    jsx: true,
+  },
   settings: {
     react: {
       version: "detect",
     },
   },
-  plugins: ["prettier"],
-  parserOptions: {
-    sourceType: "module",
+  globals: {
+    window: true,
+    document: true,
   },
   env: {
     browser: true,
@@ -24,20 +27,35 @@ module.exports = {
   },
   rules: {
     "no-console": "error",
+    "array-callback-return": "error",
+    "eqeqeq": "error",
+    "no-bitwise": "error",
+    "no-caller": "error",
+    "no-unused-expressions": "error",
     "prettier/prettier": "error",
+    "react/prop-types": "off",
+    "react/jsx-key": "error",
+    "react/no-unescaped-entities": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off",
   },
   overrides: [
     {
-      files: ["*.js", "*.jsx"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-      },
-    },
-    {
       files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+      ],
+      plugins: ["@typescript-eslint"],
       rules: {
-        "eslint(react/prop-types)": "off",
+        "no-var": "off",
+        "prefer-const": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "off",
       },
     },
   ],
